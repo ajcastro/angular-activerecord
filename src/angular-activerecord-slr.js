41d83af1b4b0cc33b250418c60b48d6e8afd5e21
Arjon Jason Castro <ajcastro29@gmail.com>
2017-01-19 08:33:00 +0800

    Fix,  move setting relations code block on top of the method

--- src/angular-activerecord-slr.js
+++ src/angular-activerecord-slr.js
@@ -7,27 +7,27 @@ angular.module('ActiveRecord').factory('ActiveRecord.Slr', ['ActiveRecord', func
     var initialize = ActiveRecord.prototype.$initialize;
 
     ActiveRecord.prototype.$initialize = function $initialize(properties, options) {
-        initialize.apply(this, arguments);
-
         /*
          * Set the declared relations on the model
          */
-        var $this = this;
-        var relations = {};
-        angular.forEach(_result(this, '$relations'), function (def, relation) {
-            var prop = def.prop||relation;
-            relations[relation] = angular.isObject($this[prop]) && !angular.isArray($this[prop]) ?
-                new def.model($this[prop]) :
-                [];
+        if (properties) {
+            var relations = {};
+            angular.forEach(_result(this, '$relations'), function (def, relation) {
+                var prop = def.prop||relation;
+                relations[relation] = angular.isObject(properties[prop]) && !angular.isArray(properties[prop]) ?
+                    new def.model(properties[prop]) :
+                    [];
 
-            if (angular.isArray(relations[relation])) {
-                angular.forEach($this[prop], function (object) {
-                    relations[relation].push(new def.model(object));
-                });
-            }
-        });
-
-        angular.extend(this, relations);
+                if (angular.isArray(relations[relation])) {
+                    angular.forEach(properties[prop], function (object) {
+                        relations[relation].push(new def.model(object));
+                    });
+                }
+            });
+            angular.extend(properties, relations);
+        } // End of setting declared relations
+        
+        initialize.apply(this, arguments);
     }
 
     return ActiveRecord;
